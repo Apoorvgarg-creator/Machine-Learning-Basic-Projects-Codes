772c23854b7175df277e22d839478994f71c1ed7
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2021-11-13 14:00:54 +0530

    module in python tutorial added

---
+++ modules/main.py
@@ -0,0 +1,12 @@
+import calculator
+from maths_appy import simple
+
+print(calculator.add(1,2))
+print(calculator.sub(1,2))
+print(calculator.mul(1,2))
+print(calculator.div(1,2))
+
+print(simple.sub(1,2))
+
+
+
